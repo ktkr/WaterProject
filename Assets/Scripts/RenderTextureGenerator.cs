8cfc130ae59a04d90a2dc574639550210fc9380d
Kevin <kangrui10@hotmail.com>
2018-04-17 12:55:57 +0800

    Added graphics script

---
+++ Assets/Scripts/RenderTextureGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextureGenerator : MonoBehaviour {
+
+    public Texture aTexture;
+    public RenderTexture rTex;
+    public Material m;
+    void Start()
+    {
+        if (!aTexture || !rTex)
+            Debug.LogError("A texture or a render texture are missing, assign them.");
+
+    }
+    void Update()
+    {
+        Graphics.Blit(aTexture, rTex,m,-1);
+        m.SetTexture("_HeightMap",rTex);
+        
+    }
+}
+
